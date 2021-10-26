25ca04fbdaf10c919c6c30182472bb0f64a03ab7
Masaru Ozawa <mo4322@gmail.com>
2021-10-26 15:32:52 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -12,16 +12,27 @@ import React, { Component } from 'react';
 //   }
 // }
 
+const profiles = [
+  {name: "Taro", age: 10},
+  {name: "Hanako", age: 11},
+  {name: "TEST"}
+]
 const App = () => {
   return <>
-    <div>Hi!</div>
-    <Cat />
-    <Cat />
+    <div>
+      {
+      profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>;
+      })
+    }
+    </div>
   </>
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name} and {props.age} years old! </div>
 }
-
+User.defaultProps = {
+  age: "1"
+};
 export default App;
