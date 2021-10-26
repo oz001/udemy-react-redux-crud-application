9abb2c5b32ca4c5d3e88c360b1d01cbedec044fc
Masaru Ozawa <mo4322@gmail.com>
2021-10-26 11:23:53 +0900

    jsk

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 class App extends React.Component {
   render() {
-    return <div><h1>Hello,world!</h1></div>
+    return <h1>Hello,world!</h1>
   }
 }
 
